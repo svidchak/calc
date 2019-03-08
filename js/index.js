8ce41866a8e95daefaac2285a5ef2ecded63bf50
slavik <slavik.fedcyshyn@gmail.com>
2019-03-08 10:51:48 +0000

    calc

---
+++ js/index.js
@@ -0,0 +1,52 @@
+function foo0() {
+    from_get = document.getElementById("id0").innerHTML;
+    alert(from_get);
+}
+
+function foo1() {
+    from_get = document.getElementById("id1").innerHTML;
+    alert(from_get);
+}
+
+function foo2() {
+    from_get = document.getElementById("id2").innerHTML;
+    alert(from_get);
+}
+
+function foo3() {
+    from_get = document.getElementById("id3").innerHTML;
+    alert(from_get);
+}
+
+function foo4() {
+    from_get = document.getElementById("id4").innerHTML;
+    alert(from_get);
+}
+
+function foo5() {
+    from_get = document.getElementById("id5").innerHTML;
+    alert(from_get);
+}
+
+function foo6() {
+    from_get = document.getElementById("id6").innerHTML;
+    alert(from_get);
+}
+
+function foo7() {
+    from_get = document.getElementById("id7").innerHTML;
+    alert(from_get);
+}
+
+function foo8() {
+    from_get = document.getElementById("id8").innerHTML;
+    alert(from_get);
+}
+
+function foo9() {
+    from_get = document.getElementById("id9").innerHTML;
+    alert(from_get);
+}
+
+
+    
